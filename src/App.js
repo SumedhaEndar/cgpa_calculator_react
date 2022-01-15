9bce9be68523a80ecec7f8233878c5dc5f39d3a7
SumedhaEndar <1211200546@student.mmu.edu.my>
2022-01-15 22:08:42 +0800

    Code 01

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import React from "react";
+import Semesters from "./CGPA_Calculator/Semesters";
+
+
+ // a dummy semNum
+ const semNum = [
+  {
+    id  : 'Sem 1',
+    sem : 1,
+    gpa : 0,
+    hours : 0 
+  }
+]
 
 function App() {
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <ul>
+        {semNum.map((eachSem)=>(
+          <Semesters semNum={eachSem}/>
+        ))}
+      </ul>
+      <div>
+        <h3><button>Add Semester</button></h3>
+      </div>
     </div>
   );
 }
---
+++ src/CGPA_Calculator/Semesters.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Semesters(props)
+{
+
+    return(
+        <div>
+            <h2>Semester {props.semNum.sem}</h2>
+        </div>
+    )
+}
+
+export default Semesters;
